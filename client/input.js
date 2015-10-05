5ab8795ce6f30a3a634dc1af6cace36fa4dc1136
Lauren Ogawa <logawa12@yahoo.com>
2015-10-05 09:10:01 -1000

    IC: W7C1 - Third Meteor App
    Slack Clone

--- client/input.js
+++ client/input.js
@@ -6,7 +6,7 @@ Template.footer.events({
   'keypress input': function(event) {
     if (event.charCode == 13) {
       event.stopPropagation();
-      $('.message-history').append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">lauren</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
+      $('.message-history').append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">username</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
       $('.input-box_text').val("");
       return false;
     }
